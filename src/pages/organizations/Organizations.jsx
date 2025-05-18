1dc332edb0c1f00b03f6b4aee2ae741de2fa3473
milan88888 <chaudharymilan996@gmail.com>
2025-05-18 12:00:13 +0545

    fix: structure

--- src/pages/organizations/Organizations.jsx
+++ src/pages/organizations/Organizations.jsx
@@ -1,15 +1,32 @@
 import { IoAdd, IoCreate, IoPulse } from "react-icons/io5";
 import { NavLink } from "react-router-dom";
+import ModuleData from "../../components/module/ModuleData";
+import Pagination from "../../components/pagination/Pagination";
+import Breadcrumb from "../../components/breadcrumb/BreadCrumb";
 
 const Organizations = () => {
   return (
     <div>
-      <NavLink to={`add-new`} className='flex flex-row justify-left'>
-        <span className="text-2xl border-1 border-green-400">
-          <IoAdd className="text-green-400" />
-        </span>
-        <span className="ml-3">Add New</span>
-      </NavLink>
+
+
+	  <div className="p-6">
+            {/* Breadcrumb with spacing */}
+            <div className='mb-8'>
+                <Breadcrumb
+                    customPaths={[
+                        { name: 'Management' },
+                        { name: 'organizations', path: null }
+                    ]}
+                />
+            </div>
+
+            {/* Module Table */}
+            <ModuleData />
+            {/* Pagination */}
+            <div className='mt-6'>
+                <Pagination />
+            </div>
+        </div>
     </div>
   );
 };
